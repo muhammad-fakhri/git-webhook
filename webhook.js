b07b35ec3d7281783faeb53571efbe6cdc756d36
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-08-15 22:34:36 +0700

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -6,8 +6,8 @@ const crypto = require('crypto');
 const exec = require('child_process').exec;
 
 http.createServer(function (req, res) {
+    console.log("Webhook running");
     req.on('data', function (chunk) {
-        console.log("Webhook running");
         let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
 
         if (req.headers['x-hub-signature'] == sig) {
