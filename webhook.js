693f553850b42e577188105f25b0454518f7ff70
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-08-15 22:45:15 +0700

    Make general

--- webhook.js
+++ webhook.js
@@ -1,16 +1,16 @@
-const secret = "indexlibrorum";
-const repo = "~/psn2020-backend/";
+const secret = "you secret";
+const repo = "path to your project dir"; // example: "~/projectA/"
 
 const http = require('http');
 const crypto = require('crypto');
 const exec = require('child_process').exec;
 
 http.createServer(function (req, res) {
-    console.log("Webhook running");
     req.on('data', function (chunk) {
         let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
-
+        
         if (req.headers['x-hub-signature'] == sig) {
+            console.log("Git webhook triggered");
             exec('cd ' + repo + ' && git pull');
         }
     });
